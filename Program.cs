6f5a821b6dd0b5b9dd30dcb96b6b17934ce658d1
Na Hyung Choi <schoinh@gmail.com>
2019-07-19 10:24:08 -0700

    add UI and input validation for taking orders

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Bakery.Models;
 
 class Program
 {
+    private static string userResponse = "";
+
     private static void ShowWelcome()
     {
         Console.Clear();
@@ -18,11 +20,57 @@ class Program
         Console.WriteLine("| Pastries          |");
         Console.WriteLine("|            $2 / 1 |");
         Console.WriteLine("|            $5 / 3 |");
-        Console.WriteLine("---------------------");
+        Console.WriteLine("---------------------\n");
+    }
+
+    private static void TakeBreadOrder()
+    {
+        Console.WriteLine("\nHow many loaves of bread would you like?"); 
+        Console.Write("Enter number of loaves: ");
+        string numBread = Console.ReadLine();
+
+        // Validate user input and convert input to integer
+        int intNumBread;
+        bool isInt = int.TryParse(numBread, out intNumBread);
+        if (!isInt)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please enter a valid number.");
+            Console.ResetColor();
+            TakeBreadOrder();
+        }
+    }
+
+    private static void TakePastryOrder()
+    {
+        Console.WriteLine("\nHow many pastries would you like?");
+        Console.Write("Enter number of pastries: ");
+        string numPastry = Console.ReadLine();
+
+        // Validate user input and convert input to integer
+        int intNumPastry;
+        bool isInt = int.TryParse(numPastry, out intNumPastry);
+        if (!isInt)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please enter a valid number.");
+            Console.ResetColor();
+            TakePastryOrder();
+        }
     }
     
     private static void Main()
     {
         ShowWelcome();
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("1: Place an order");
+        Console.Write("\nEnter Option (1-): ");
+        userResponse = Console.ReadLine();
+        
+        if (userResponse == "1")
+        {
+            TakeBreadOrder();
+            TakePastryOrder();
+        }
     }
 }
