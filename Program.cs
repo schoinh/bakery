2921e6462b589628c0ade6d3249eee55a2a32ddc
Na Hyung Choi <schoinh@gmail.com>
2019-07-19 09:14:42 -0700

    add welcome message and menu

--- Program.cs
+++ Program.cs
@@ -3,8 +3,26 @@ using Bakery.Models;
 
 class Program
 {
+    private static void ShowWelcome()
+    {
+        Console.Clear();
+        Console.WriteLine("#~#~#~#~#~#~#~#~#~#~#~#");
+        Console.WriteLine("#  BAGHEERA'S BAKERY  #");
+        Console.WriteLine("#~#~#~#~#~#~#~#~#~#~#~#");
+        Console.WriteLine("\n~ Bienvenu a la Boulangerie ~\n");
+
+        Console.WriteLine("------- MENU --------");
+        Console.WriteLine("| Wheat Bread       |");
+        Console.WriteLine("|            $5 / 1 |");
+        Console.WriteLine("|           $10 / 3 |");
+        Console.WriteLine("| Pastries          |");
+        Console.WriteLine("|            $2 / 1 |");
+        Console.WriteLine("|            $5 / 3 |");
+        Console.WriteLine("---------------------");
+    }
+    
     private static void Main()
     {
-        
+        ShowWelcome();
     }
 }
