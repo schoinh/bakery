3ec089bb18f73f67b7f711fa5e1a2608d1e9fc55
Na Hyung Choi <schoinh@gmail.com>
2019-07-19 12:33:54 -0700

    add option to keep adding to order

--- Program.cs
+++ Program.cs
@@ -32,12 +32,10 @@ class Program
 
     private static void TakeBreadOrder()
     {
-        Console.WriteLine("\nHow many loaves of bread would you like?"); 
-        Console.Write("Enter number of loaves: ");
+        Console.Write("\nHow many loaves of bread would you like?: "); 
         string numBread = Console.ReadLine();
 
         // Validate user input and convert input to integer
-        // int intNumBread;
         bool isInt = int.TryParse(numBread, out intNumBread);
         if (!isInt)
         {
@@ -50,12 +48,10 @@ class Program
 
     private static void TakePastryOrder()
     {
-        Console.WriteLine("\nHow many pastries would you like?");
-        Console.Write("Enter number of pastries: ");
+        Console.Write("\nHow many pastries would you like?: ");
         string numPastry = Console.ReadLine();
 
         // Validate user input and convert input to integer
-        // int intNumPastry;
         bool isInt = int.TryParse(numPastry, out intNumPastry);
         if (!isInt)
         {
@@ -68,8 +64,8 @@ class Program
 
     private static void ProcessOrder()
     {
-        breadOrder.Quantity = intNumBread;
-        pastryOrder.Quantity = intNumPastry;
+        breadOrder.Quantity += intNumBread;
+        pastryOrder.Quantity += intNumPastry;
 
         breadOrder.GetCost();
         pastryOrder.GetCost();
@@ -82,12 +78,78 @@ class Program
         int totalCost = breadOrder.Cost + pastryOrder.Cost;
 
         Console.WriteLine("\n------- YOUR ORDER -------");
-        Console.WriteLine($" {intNumBread}x Wheat Bread");
-        Console.WriteLine($"                    ${breadOrder.Cost}.00");
-        Console.WriteLine($" {intNumPastry}x Pastries");
-        Console.WriteLine($"                    ${pastryOrder.Cost}.00");
+        Console.WriteLine($" {breadOrder.Quantity}x Wheat Bread");
+        Console.WriteLine($"                   ${breadOrder.Cost}.00");
+        Console.WriteLine($" {pastryOrder.Quantity}x Pastries");
+        Console.WriteLine($"                   ${pastryOrder.Cost}.00");
         Console.WriteLine("--------------------------");
-        Console.WriteLine($"             TOTAL: ${totalCost}.00\n");
+        Console.WriteLine($"            TOTAL: ${totalCost}.00\n");
+
+        AddToOrder();
+    }
+
+    private static void AddToOrder()
+    {
+        Console.WriteLine("Would you like to add any more items to your order?");
+        Console.WriteLine(" 1: More Bread\n 2: More Pastries\n 3: No Thanks, I'm done");
+        Console.Write("\nYour Choice (1-3): ");
+        userResponse = Console.ReadLine();
+
+        switch (userResponse)
+        {
+            case "1":
+                AddMoreBread();
+                break;
+            case "2":
+                AddMorePastry();
+                break;
+            case "3":
+                Console.WriteLine("Thank you for visiting!");
+                Environment.Exit(0);
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a number between 1 and 3.");
+                Console.ResetColor();
+                AddToOrder();
+                break;
+        }
+    }
+
+    private static void AddMoreBread()
+    {
+        Console.Write("\nHow many more loaves of bread?: ");
+        string numBread = Console.ReadLine();
+
+        // Validate user input and convert input to integer
+        bool isInt = int.TryParse(numBread, out intNumBread);
+        if (!isInt)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please enter a valid number.");
+            Console.ResetColor();
+            AddMoreBread();
+        }
+
+        ProcessOrder();
+    }
+
+    private static void AddMorePastry()
+    {
+        Console.Write("\nHow many more pastries?: ");
+        string numPastry = Console.ReadLine();
+
+        // Validate user input and convert input to integer
+        bool isInt = int.TryParse(numPastry, out intNumPastry);
+        if (!isInt)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please enter a valid number.");
+            Console.ResetColor();
+            AddMorePastry();
+        }
+
+        ProcessOrder();
     }
     
     private static void Main()
@@ -104,7 +166,7 @@ class Program
         }
         else
         {
-            Console.WriteLine("\nThank you for visiting!");
+            Console.WriteLine("\nGreat! Thank you for visiting!");
         }
     }
 }
