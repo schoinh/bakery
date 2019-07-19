2893743f5ec2a3298a79d1c592617bbc02cde7dd
Na Hyung Choi <schoinh@gmail.com>
2019-07-19 15:23:23 -0700

    create BakedGood class to be parent class for Bread and Pastry

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,19 +1,19 @@
-using System;
-using System.Collections.Generic;
-
 namespace Bakery.Models
 {
-    class Bread
+    class BakedGood
     {
         public int Quantity { get; set; }
         public int Cost { get; set; }
         
-        public Bread()
+        public BakedGood()
         {
             Quantity = 0;
             Cost = 0;
         }
+    }
 
+    class Bread : BakedGood
+    {
         public void GetCost()
         { 
             int numThrees = Quantity / 3;
@@ -23,17 +23,8 @@ namespace Bakery.Models
         }
     }
 
-    class Pastry
+    class Pastry : BakedGood
     {
-        public int Quantity { get; set; }
-        public int Cost { get; set; }
-        
-        public Pastry()
-        {
-            Quantity = 0;
-            Cost = 0;
-        }
-
         public void GetCost()
         { 
             int numThrees = Quantity / 3;
